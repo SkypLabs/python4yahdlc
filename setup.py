aae6f0031edad0ed8d5c540e638ff7502112b096
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-12-10 07:54:53 +0100

    update 'setup.py'

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
 	name = 'python4yahdlc',
 	version = '0.1.0',
 	description = 'Python bindings for the yahdlc library',
+	license = 'GPLv3',
+	keywords = 'hdlc yahdlc bindings',
 	author = 'Paul-Emmanuel Raoul',
 	author_email = 'skyper@skyplabs.net',
 	url = 'https://github.com/SkypLabs/python4yahdlc',
