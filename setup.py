499bc5b1c8084ae9bd53c883e9e70c98bc0b6e16
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2016-10-02 18:47:33 +0100

    add classifiers to 'setup.py'

--- setup.py
+++ setup.py
@@ -33,6 +33,18 @@ setup(
     author_email = 'skyper@skyplabs.net',
     url = 'https://github.com/SkypLabs/python4yahdlc',
     download_url = 'https://github.com/SkypLabs/python4yahdlc/archive/v{0}.zip'.format(VERSION),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
     ext_modules = [yahdlc],
     test_suite = 'test',
 )
